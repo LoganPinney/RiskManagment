91a3ffd0507c7ad37b8850a71919dbb8e1516395
Logan Pinney <97063910+LoganPinney@users.noreply.github.com>
2025-07-15 15:34:49 -0600

    making ui open

--- electron-main.js
+++ electron-main.js
@@ -1,20 +1,26 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
-const { join } = require('path');
+const path = require('path');
 const fs = require('fs');
+
 const userDataPath = app.getPath('userData');
-const presetsFile = join(userDataPath, 'presets.json');
+const presetsFile = path.join(userDataPath, 'presets.json');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false
     }
   });
-  win.loadURL('http://localhost:5173');
+
+  if (process.env.NODE_ENV !== 'production') {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadFile(path.join(__dirname, 'build', 'index.html'));
+  }
 }
 
 app.whenReady().then(() => {
@@ -26,7 +32,9 @@ app.whenReady().then(() => {
 
 ipcMain.handle('load-presets', async () => {
   try {
-    if (!fs.existsSync(presetsFile)) fs.writeFileSync(presetsFile, JSON.stringify({}));
+    if (!fs.existsSync(presetsFile)) {
+      fs.writeFileSync(presetsFile, JSON.stringify({}));
+    }
     const raw = fs.readFileSync(presetsFile);
     return JSON.parse(raw);
   } catch (e) {
@@ -38,3 +46,4 @@ ipcMain.handle('save-presets', async (_, data) => {
   fs.writeFileSync(presetsFile, JSON.stringify(data, null, 2));
   return true;
 });
+package.json
